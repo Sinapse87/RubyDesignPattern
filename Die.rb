13a36ab0c71ab00dc922b0f9175c44671d854054
Sinapse87 <lucali87@gmail.com>
2014-11-16 17:15:47 +0000

    Global refactoring

--- Die.rb
+++ Die.rb
@@ -1,29 +1,29 @@
 class Die
 
   attr_accessor :faces
-  attr_accessor :throwHash
+  attr_accessor :throwStat
   attr_accessor :throwCount
 
 
   def initialize
-    @faces = [:north,:south,:east,:west];
-    @throwHash = { :north => 0, :south => 0, :east => 0, :west =>0}
+    @faces = [:north,:south,:east,:west]
+    @throwStat = { :north => 0, :south => 0, :east => 0, :west =>0}
     @throwCount=0
   end
 
   public
   def throw
-    face = [:north,:south,:east,:west].sample()
-    @throwHash[face] += 1
+    face = @faces.sample()
+    @throwStat[face] += 1
     @throwCount+=1
     return face
   end
 
   public
   def stats
-    @throwHash.each do |key, value|
+    @throwStat.each do |key, value|
       print "#{key} :"
-      print "#{value*100/@throwCount}% "
+      print "#{(value*100/@throwCount)}% "
     end
   end
 
--- Point.rb
+++ Point.rb
@@ -4,21 +4,12 @@ class Point
   attr_accessor :y
 
   def initialize (x,y)
-    if (x>=0 && x<10 && y>=0 && y<10)
       @x=x;
       @y=y;
-    else
-      puts "Coordinates are wrong. The point will not be instantiate"
-      return null
-    end
   end
 
   public
   def == (otherPoint)
-    if ((@x==otherPoint.x) && (@y==otherPoint.y))
-      return true
-    else
-      return false
-    end
+    return (@x==otherPoint.x) && (@y==otherPoint.y)
   end
 end
